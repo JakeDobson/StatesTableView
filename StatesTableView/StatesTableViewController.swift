51d1297fb8376d81adbe90eec27b0f2e858d6d15
Jacob Dobson <jacobdobson@Jacobs-MacBook-Air.local>
2016-05-31 15:38:30 -0600

    restarted project over

---
+++ StatesTableView/StatesTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  StatesTableViewController.swift
+//  StatesTableView
+//
+//  Created by Jacob Dobson on 5/31/16.
+//  Copyright © 2016 Jacob Dobson. All rights reserved.
+//
+
+import UIKit
+
+class StatesTableViewController: UITableViewController {
+    
+    let fiftyStates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        //return the number of sections
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return fiftyStates.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        let state = fiftyStates[indexPath.row]
+        
+        cell.textLabel?.text = state
+        
+        return cell
+    }
+}
